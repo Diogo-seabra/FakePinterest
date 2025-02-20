c046aa09d3c6c04ea4478b62621d1defaf07f7b9
Diogo Seabra <seabradiogo33@gmail.com>
2025-02-19 21:58:16 -0300

    Implementar o formulário de login na homepage e de criar conta

--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -4,13 +4,13 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 from fakepinterest.models import Usuario
 
-class FormLogin():
-    email = StringField("E=mail", validators=[DataRequired(), Email()])
+class FormLogin(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
     senha = PasswordField("Senha", validators=[DataRequired()]) 
     botao_confirmacao = SubmitField("Fazer Login")
 
-class FormCriarConta():
-    email = StringField("E=mail", validators=[DataRequired(), Email()])
+class FormCriarConta(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
     username = StringField("Nome de usuário", validators=[DataRequired()])
     senha = PasswordField("Senha", validators=[DataRequired(), Length(6, 20)])
     confirmacao_senha = PasswordField("Confirmação de Senha", validators=[DataRequired(), EqualTo("senha")])
